b57ec282ba8db5c9d8115322dc406b0a3ceda183
aleksei-manikhin <138869287+AlexMani1501@users.noreply.github.com>
2026-03-29 22:24:08 +0300

    add Column struct in demography_record

--- demography_record.h
+++ demography_record.h
@@ -2,6 +2,19 @@
 #define DEMOGRAPHY_RECORD_H
 
 #define REGION_SIZE 128
+
+
+typedef enum {
+    COL_YEAR = 1,
+    COL_REGION = 2,
+    COL_NPG = 3,
+    COL_BIRTH_RATE = 4,
+    COL_DEATH_RATE = 5,
+    COL_GDW = 6,
+    COL_URBANIZATION = 7
+} Column;
+
+
 typedef struct
 {
     int year;
